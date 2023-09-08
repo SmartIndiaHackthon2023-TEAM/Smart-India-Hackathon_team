0ed7d2fe12ab8c09a8bfdf885ec0cd6c6ee8a291
Shashwat Singh <shashwat123student@gmail.com>
2023-09-08 20:54:06 +0530

    Frontend fully working added all the basic routes/webpages

--- SIH_Web/src/App.jsx
+++ SIH_Web/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from "./Signup.jsx";
+import SignIn from "./SignIn.jsx";
+import {CheckUrl} from "./CheckUrl.jsx";
+import {LandingPage} from "./LandingPage.jsx";
+import {CheckEmail} from "./CheckEmail.jsx";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    return (
+            <div>
+                <Router>
+                    <Routes>
+                        <Route path={"/"} element={<LandingPage />} />
+                        <Route path={"/email"} element={<CheckEmail />} />
+                        <Route path={"/url"} element={<CheckUrl />} />
+                        <Route path={"/signin"} element={<SignIn />} />
+                        <Route path={"/signup"} element={<Signup />} />
+                    </Routes>
+                </Router>
+            </div>
   )
 }
 
---
+++ SIH_Web/src/CheckUrl.jsx
@@ -0,0 +1,7 @@
+export function CheckUrl(){
+    return(
+        <div>
+            <h1>Url Checking Webpage</h1>
+        </div>
+    )
+}
