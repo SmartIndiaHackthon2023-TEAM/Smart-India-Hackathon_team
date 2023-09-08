5bcccece4567f964a097dd82c594bdfebfaad0f8
Shashwat Singh <shashwat123student@gmail.com>
2023-09-09 01:41:24 +0530

    Integration

---
+++ main.py
@@ -0,0 +1,191 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import numpy as np
+import pickle
+from tld import get_tld
+from urllib.parse import urlparse
+import re
+from googlesearch import search
+
+app = FastAPI()
+
+# Load your trained model
+with open('xgb_c_pkl', 'rb') as f:
+    mod = pickle.load(f)
+
+class URLInput(BaseModel):
+    url: str
+
+@app.post("/predict/")
+async def predict(url_input: URLInput):
+    try:
+        url = url_input.url
+        prediction = get_prediction_from_url(url)
+        return {"url": url, "prediction": prediction}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+# URL analysis functions
+def count_www(url):
+    return url.count('www')
+
+def count_atrate(url):
+    return url.count('@')
+
+def no_of_dir(url):
+    urldir = urlparse(url).path
+    return urldir.count('/')
+
+def no_of_embed(url):
+    urldir = urlparse(url).path
+    return urldir.count('//')
+
+def shortening_service(url):
+    match = re.search('bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|'
+                      'yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|'
+                      'short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|'
+                      'doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|'
+                      'db\.tt|qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|'
+                      'q\.gs|is\.gd|po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|'
+                      'x\.co|prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|'
+                      'tr\.im|link\.zip\.net',
+                      url)
+    if match:
+        return 1
+    else:
+        return 0
+
+def count_http(url):
+    return url.count('http')
+
+def count_https(url):
+    return url.count('https')
+
+def count_per(url):
+    return url.count('%')
+
+def count_ques(url):
+    return url.count('?')
+
+def count_hyphen(url):
+    return url.count('-')
+
+def count_equal(url):
+    return url.count('=')
+
+def url_length(url):
+    return len(str(url))
+
+def hostname_length(url):
+    return len(urlparse(url).netloc)
+
+def suspicious_words(url):
+    match = re.search('PayPal|login|signin|bank|account|update|free|lucky|service|bonus|ebayisapi|webscr',
+                      url)
+    if match:
+        return 1
+    else:
+        return 0
+
+def digit_count(url):
+    digits = 0
+    for i in url:
+        if i.isnumeric():
+            digits = digits + 1
+    return digits
+
+def letter_count(url):
+    letters = 0
+    for i in url:
+        if i.isalpha():
+            letters = letters + 1
+    return letters
+
+# First Directory Length
+def fd_length(url):
+    urlpath = urlparse(url).path
+    try:
+        return len(urlpath.split('/')[1])
+    except:
+        return 0
+
+def tld_length(tld):
+    try:
+        return len(tld)
+    except:
+        return -1
+
+def count_dot(url):
+    count_dot = url.count('.')
+    return count_dot
+
+def google_index(url):
+    site = search(url, 5)
+    return 1 if site else 0
+
+def having_ip_address(url):
+    match = re.search(
+        '(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.'
+        '([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\/)|'  # IPv4
+        '((0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\.(0x[0-9a-fA-F]{1,2})\\/)' # IPv4 in 16-bit format
+        '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}', url)  # IPv6
+    if match:
+        return 1
+    else:
+        return 0
+
+def abnormal_url(url):
+    hostname = urlparse(url).hostname
+    hostname = str(hostname)
+    match = re.search(hostname, url)
+    if match:
+        return 1
+    else:
+        return 0
+
+def get_prediction_from_url(test_url):
+    features_test = main(test_url)
+    # Due to updates to scikit-learn, we now need a 2D array as a parameter to the predict function.
+    features_test = np.array(features_test).reshape((1, -1))
+
+    pred = mod.predict(features_test)
+    if int(pred[0]) == 0:
+        res = "SAFE"
+        return res
+    elif int(pred[0]) == 1.0:
+        res = "DEFACEMENT"
+        return res
+    elif int(pred[0]) == 2.0:
+        res = "PHISHING"
+        return res
+    elif int(pred[0]) == 3.0:
+        res = "MALWARE"
+        return res
+
+def main(url):
+    status = []
+
+    status.append(having_ip_address(url))
+    status.append(abnormal_url(url))
+    status.append(count_dot(url))
+    status.append(count_www(url))
+    status.append(count_atrate(url))
+    status.append(no_of_dir(url))
+    status.append(no_of_embed(url))
+
+    status.append(shortening_service(url))
+    status.append(count_https(url))
+    status.append(count_http(url))
+
+    status.append(fd_length(url))
+    tld = get_tld(url, fail_silently=True)
+
+    status.append(tld_length(tld))
+
+    return status
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
