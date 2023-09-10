a35e109b5f2566c6020b79b9d97ac6622d50f225
Shashwat Singh <shashwat123student@gmail.com>
2023-09-10 15:36:26 +0530

    Beautifying the signin and signup page

--- SIH_Web/src/SignIn.jsx
+++ SIH_Web/src/SignIn.jsx
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from "axios";
+import {
+    Avatar,
+    Box,
+    createTheme,
+    CssBaseline,
+    Grid,
+    Link,
+    Paper,
+    TextField,
+    ThemeProvider,
+    Typography
+} from "@mui/material";
 import {useState} from "react";
+import axios from "axios";
 
 function Copyright(props) {
     return (
@@ -27,92 +28,114 @@ function Copyright(props) {
 }
 
 const defaultTheme = createTheme();
-
-export default function SignIn() {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        paddingTop: 29,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'black' , width : 60 , height : 60}}>
-                        _Team
-                    </Avatar>
-                    <Typography component="h1" variant="h5" style={{color : "#292929"}}>
-                        Welcome Back
-                    </Typography>
-                    <Box component="form" noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Email Address"
-                            autoComplete="email"
-                            autoFocus
-                            onChange={(e)=>{
-                                setEmail(e.target.value);
-                            }}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                            onChange={(e)=>{
-                                setPassword(e.target.value);
-                            }}
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={ async () => {
-                                try {
-                                    let sendData = {
-                                        username : email,
-                                        password : password
-                                    }
-                                    const res = await axios.post("http://localhost:3000/login",sendData,{
-                                        headers: {
-                                            "Content-Type": "application/json",
-                                        },
-                                    });
-                                    const data = res.data;
-                                    localStorage.setItem("token", data.token);
-                                    window.location = "/";
-                                } catch (error) {
-                                    console.error();
-                                }
+function SignIn(){
+    const [email,setEmail] =useState("");
+    const [password,setPassword]=useState("");
+    return(
+        <div>
+            <ThemeProvider theme={defaultTheme}>
+                <Grid container component="main" sx={{ height: '100vh' }}>
+                    <CssBaseline />
+                    <Grid
+                        item
+                        xs={false}
+                        sm={4}
+                        md={7}
+                        sx={{
+                            backgroundImage: 'url(https://about.coursera.org/static/whiteCoursera-23ec484f7091914430ce19b07d09aedf.svg)',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    />
+                    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                height: '100vh',
                             }}
                         >
-                            Sign In
-                        </Button>
-                        <Grid container> 
-                            <Grid item style={{
-                                paddingLeft: "95px",
-                            }}>
-                                <Link href="#" variant="body2" onClick={() => {
-                                    window.location.href = "./signup";
-                                }}>
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
-    );
+                            <Avatar sx={{ m: 1, bgcolor: 'black' , width : 60 , height : 60}}>
+                                _Team
+                            </Avatar>
+                            <Typography component="h1" variant="h5" style={{color : "#292929"}}>
+                                Welcome Back
+                            </Typography>
+                            <Box component="form" noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    label="Email Address"
+                                    autoComplete="email"
+                                    autoFocus
+                                    onChange={(e)=>{
+                                        setEmail(e.target.value);
+                                    }}
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    label="Password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    onChange={(e)=>{
+                                        setPassword(e.target.value);
+                                    }}
+                                />
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    onClick={ async () => {
+                                        try {
+                                            let sendData = {
+                                                username : email,
+                                                password : password
+                                            }
+                                            const res = await axios.post("http://localhost:3000/login",sendData,{
+                                                headers: {
+                                                    "Content-Type": "application/json",
+                                                },
+                                            });
+                                            const data = res.data;
+                                            localStorage.setItem("token", data.token);
+                                            window.location = "/";
+                                        } catch (error) {
+                                            console.error();
+                                        }
+                                    }}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item style={{
+                                        paddingLeft: "95px",
+                                    }}>
+                                        <div
+                                            style={{
+                                                paddingLeft: 50,
+                                                paddingTop: 15,
+                                                cursor: 'pointer',
+                                            }}
+                                            onClick={() => {
+                                                window.location.href = "./signup";
+                                            }}
+                                        >
+                                            Don't have an account? Sign Up
+                                        </div>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </ThemeProvider>
+        </div>
+    )
 }
+export default SignIn;
--- SIH_Web/src/Signup.jsx
+++ SIH_Web/src/Signup.jsx
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from "axios";
+import {
+    Avatar,
+    Box,
+    createTheme,
+    CssBaseline,
+    Grid,
+    Link,
+    Paper,
+    TextField,
+    ThemeProvider,
+    Typography
+} from "@mui/material";
 import {useState} from "react";
+import axios from "axios";
 
 function Copyright(props) {
     return (
@@ -27,95 +28,116 @@ function Copyright(props) {
 }
 
 const defaultTheme = createTheme();
-
-export default function Signup() {
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        paddingTop: 29,
-                        marginTop: 0,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'black' , width : 60 , height : 60}}>
-                        _Team
-                    </Avatar>
-                    <Typography component="h1" variant="h5" color= "#292929">
-                        Welcome
-                    </Typography>
-                    <Box component="form" noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Email Address"
-                            autoComplete="email"
-                            autoFocus
-                            onChange={(e)=>{
-                                setEmail(e.target.value);
-                            }}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                            onChange={(e)=>{
-                                setPassword(e.target.value);
-                            }}
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={async () => {
-                                try {
-                                    const requestData = {
-                                        username: email,
-                                        password: password,
-                                    };
-
-                                    const res = await axios.post("http://localhost:3000/signup", requestData, {
-                                        headers: {
-                                            "Content-Type": "application/json",
-                                        },
-                                    });
-
-                                    const data = res.data;
-                                    localStorage.setItem("token", data.token);
-                                    window.location = "/";
-                                } catch (error) {
-                                    console.error("Error:", error);
-                                }
+function SignIn(){
+    const [email,setEmail] =useState("");
+    const [password,setPassword]=useState("");
+    return(
+        <div>
+            <ThemeProvider theme={defaultTheme}>
+                <Grid container component="main" sx={{ height: '100vh' }}>
+                    <CssBaseline />
+                    <Grid
+                        item
+                        xs={false}
+                        sm={4}
+                        md={7}
+                        sx={{
+                            backgroundImage: 'url(https://about.coursera.org/static/whiteCoursera-23ec484f7091914430ce19b07d09aedf.svg)',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}
+                    />
+                    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                height: '100vh',
                             }}
                         >
-                            Sign Up
-                        </Button>
-                        <Grid container>
-                            <Grid item style={{
-                                paddingLeft: "95px",
-                            }}>
-                                <Link href="#" variant="body2" onClick={ ()=>{
-                                    window.location="/signin";
-                                }}>
-                                    {"Already have an account? Sign In"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
-    );
+                            <Avatar sx={{ m: 1, bgcolor: 'black' , width : 60 , height : 60}}>
+                                _Team
+                            </Avatar>
+                            <Typography component="h1" variant="h5" style={{color : "#292929"}}>
+                                Welcome
+                            </Typography>
+                            <Box component="form" noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    label="Email Address"
+                                    autoComplete="email"
+                                    autoFocus
+                                    onChange={(e)=>{
+                                        setEmail(e.target.value);
+                                    }}
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    label="Password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    onChange={(e)=>{
+                                        setPassword(e.target.value);
+                                    }}
+                                />
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    onClick={async () => {
+                                        try {
+                                            const requestData = {
+                                                username: email,
+                                                password: password,
+                                            };
+
+                                            const res = await axios.post("http://localhost:3000/signup", requestData, {
+                                                headers: {
+                                                    "Content-Type": "application/json",
+                                                },
+                                            });
+
+                                            const data = res.data;
+                                            localStorage.setItem("token", data.token);
+                                            window.location = "/";
+                                        } catch (error) {
+                                            console.error("Error:", error);
+                                        }
+                                    }}
+                                >
+                                    Sign Up
+                                </Button>
+                                <Grid container>
+                                    <Grid item style={{
+                                        paddingLeft: "95px",
+                                    }}>
+                                        <div
+                                            style={{
+                                                paddingLeft: 50,
+                                                paddingTop: 15,
+                                                cursor: 'pointer',
+                                            }}
+                                            onClick={() => {
+                                                window.location.href = "./signup";
+                                            }}
+                                        >
+                                            Already have an account ? Sign In
+                                        </div>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </ThemeProvider>
+        </div>
+    )
 }
+export default SignIn;
